47eda11a5b67238a7a24c9acdb0d44e41c795031
DimasWahyuH <dimaswh19@gmail.com>
2024-05-20 19:54:05 +0700

    Menambahkan class public orang

--- ParadigmaOOP1_0078/Inheritance.cpp
+++ ParadigmaOOP1_0078/Inheritance.cpp
@@ -44,3 +44,12 @@ public:
         return "Hai, nama saya " + nama + "dari sekolah" + sekolah + "\n\n";
     }
 };
+
+int main()
+{
+    pelajar siswa1("andi laksana", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
