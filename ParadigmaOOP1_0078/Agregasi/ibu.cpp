79109ad17fcd77b52964fd5dd5947a333785199c
DimasWahyuH <dimaswh19@gmail.com>
2024-05-20 19:27:42 +0700

    Menambahkan class ibu

--- ParadigmaOOP1_0078/Agregasi/ibu.cpp
+++ ParadigmaOOP1_0078/Agregasi/ibu.cpp
@@ -2,16 +2,14 @@
 #include <vector>
 using namespace std;
 
-#include "anak.h"
-#include "ibu.h"
+#include "anak.H"
+#include "ibu.H"
 
 int main()
 {
-
     ibu *varIbu = new ibu("dini");
     ibu *varIbu2 = new ibu("novi");
-
-    anak *varAnak1 = new anak("tomo");
+    anak *varAnak1 = new anak("tono");
     anak *varAnak2 = new anak("rini");
     anak *varAnak3 = new anak("dewi");
 
@@ -27,4 +25,6 @@ int main()
     delete varIbu2;
     delete varAnak1;
     delete varAnak2;
+    delete varAnak3;
+
 }
